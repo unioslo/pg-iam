36eb359675a621ddbb7d97a86de0713d467811c1
leondutoit <dutoit.leon@gmail.com>
2023-03-12 20:04:36 +0100

    implement day of week plus time of day constraints on group membership

--- mgrt/2.2-to-2.3.sql
+++ mgrt/2.2-to-2.3.sql
@@ -1,3 +1,4 @@
 
 alter table group_memberships add column if not exists start_date timestamptz;
 alter table group_memberships add column if not exists end_date timestamptz;
+alter table group_memberships add column if not exists weekdays jsonb;
