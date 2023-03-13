f92c58f02129bef5c17c14cca6102b80d742abc3
leondutoit <dutoit.leon@gmail.com>
2023-03-13 14:51:50 +0100

    add check constraint to migration script

--- mgrt/2.2-to-2.3.sql
+++ mgrt/2.2-to-2.3.sql
@@ -2,6 +2,7 @@
 alter table group_memberships add column if not exists start_date timestamptz;
 alter table group_memberships add column if not exists end_date timestamptz;
 alter table group_memberships add column if not exists weekdays jsonb;
+alter table group_memberships add constraint group_memberships_check check (start_date < end_date);
 
 alter table audit_log_relations add column if not exists start_date timestamptz;
 alter table audit_log_relations add column if not exists end_date timestamptz;
