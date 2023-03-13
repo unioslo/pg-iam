a23c0ecb011174759453d0e4ed963435305453ab
leondutoit <dutoit.leon@gmail.com>
2023-03-13 09:08:08 +0100

    add new columns to audit_log_relation in migration script

--- mgrt/2.2-to-2.3.sql
+++ mgrt/2.2-to-2.3.sql
@@ -2,3 +2,7 @@
 alter table group_memberships add column if not exists start_date timestamptz;
 alter table group_memberships add column if not exists end_date timestamptz;
 alter table group_memberships add column if not exists weekdays jsonb;
+
+alter table audit_log_relations add column if not exists start_date timestamptz;
+alter table audit_log_relations add column if not exists end_date timestamptz;
+alter table audit_log_relations add column if not exists weekdays jsonb;
